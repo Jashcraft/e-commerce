92e8c937872729b32ea0c250567eff1fc11e96c9
Jashcraft <ashcraft.jonathon@gmail.com>
2022-06-03 19:33:47 -0600

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -20,25 +20,25 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL(12,2),
+      type: DataTypes.DECIMAL(12, 2),
       allowNull: false,
       validate: {
         isDecimal: true
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10,
-        validate: {
-          isNumeric: true
-        }
-      },
-      category_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'category',
-          key: 'id',
-        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
       }
     }
   },
